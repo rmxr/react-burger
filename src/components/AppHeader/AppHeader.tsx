c9fc55d4bebbdeba65bbe0c2077b2f079457176f
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-02-10 14:24:11 +0300

    feat: make inactive header buttons

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import {ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
-import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./AppHeader.module.css";
 import HeaderButton from "../HeaderButton/HeaderButton";
 
@@ -9,17 +8,13 @@ function AppHeader() {
         <header className={styles.header}>
             <div className={styles.headerGrid}>
                 <div className={styles.leftBlock}>
-                    <HeaderButton text={"Конструктор"}>
-                        <BurgerIcon type="primary"/>
-                    </HeaderButton>
-                    <HeaderButton text={"Лента заказов"}>
-                        <ListIcon type="primary"/>
-                    </HeaderButton>
+                    <HeaderButton text={"Конструктор"} type={"burger"} active={true} />
+                    <HeaderButton text={"Лента заказов"} type={"list"} active={false} />
                 </div>
                 <Logo/>
-                <div className={styles.rightBlock}><HeaderButton text={"Личный кабинет"}>
-                    <ProfileIcon type="primary"/>
-                </HeaderButton></div>
+                <div className={styles.rightBlock}>
+                    <HeaderButton text={"Личный кабинет"} type={"profile"} active={false}/>
+                </div>
             </div>
         </header>
     )
--- src/components/HeaderButton/HeaderButton.tsx
+++ src/components/HeaderButton/HeaderButton.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styles from "./HeaderButton.module.css";
+import {BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
 interface ButtonProps {
     text: string;
-    children?: React.ReactNode;
+    type: string;
+    active: boolean;
 }
+
+const components: {[index: string]:any} = {
+    burger: BurgerIcon,
+    list: ListIcon,
+    profile: ProfileIcon,
+};
 function HeaderButton(props: ButtonProps) {
+    const SpecificIcon = components[props.type];
+
     return(
         <div className={`${styles.button} pl-5 pr-5 mt-4 mb-4`}>
-            {props.children}
-            <p className="ml-2 text text_type_main-default">{props.text}</p>
+            <SpecificIcon type={props.active ? "primary" : "secondary"} />
+            <p className={`${!props.active && "text_color_inactive"} ml-2 text text_type_main-default`}>{props.text}</p>
         </div>
     )
 }
