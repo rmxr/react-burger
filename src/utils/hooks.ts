409ebf84eccc755adfd767719674713b8abcc999
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-03-22 17:25:46 +0300

    feat: convert store to TS

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -3,7 +3,9 @@ import {TRootState} from "../index";
 import {AppDispatch} from "../index";
 import {AppThunk} from "./types";
 
+// export const useAppDispatch = () => useDispatch<AppDispatch | AppThunk>();
+// export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector;
 
-export const useAppDispatch = () => useDispatch<AppDispatch | AppThunk>();
-// export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector;
+type DispatchFunc = () => AppDispatch | AppThunk;
+export const useAppDispatch: DispatchFunc = useDispatch
+export const useAppSelector: TypedUseSelectorHook<TRootState> = useSelector
