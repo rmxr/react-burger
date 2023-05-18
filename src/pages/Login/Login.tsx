4d3243f05135a0fdf585195061a449bd4d7de7a4
Anton Voytsekhovskiy <rmxr@yandex.ru>
2023-05-18 23:06:40 +0300

    fix: login validation

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, {FormEventHandler} from 'react';
+import React, {FormEventHandler, useRef} from 'react';
 import styles from "./Login.module.css";
 import {Input, Button} from "@ya.praktikum/react-developer-burger-ui-components";
 import {Link} from "react-router-dom";
@@ -9,30 +9,56 @@ import {useAppDispatch, useForm} from "../../utils/hooks";
 function Login() {
   const [value, handleChange] = useForm({"E-mail": "", "Password": ""});
   const [type, setType] = React.useState<"password" | "text" | "email">('password');
+  const [error, setError] = React.useState(false);
+  const [isFormValid, setFormValidity] = React.useState<boolean | undefined>(false)
   const dispatch = useAppDispatch();
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const keyframes = [
+    {transform: "translateX(0)"},
+    {transform: "translateX(5px)"},
+    {transform: "translateX(-5px)"},
+    {transform: "translateX(5px)"},
+    {transform: "translateX(0)"},
+  ];
 
 
-  const submitHandler: FormEventHandler = (e) => {
+  const submitHandler: FormEventHandler = async (e) => {
     e.preventDefault();
-    dispatch(login(value["E-mail"], value.Password));
+    try {
+      await dispatch(login(value["E-mail"], value.Password))
+    } catch (error) {
+      formRef.current?.animate(keyframes, {
+        duration: 150,
+        iterations: 3,
+      });
+      setError(true);
+      setTimeout(() => {
+        setError(false)
+      }, 2500)
+    }
   };
 
+  const inputHandler: FormEventHandler = () => setFormValidity(formRef.current?.checkValidity())
 
   return (
     <main className={styles.main}>
       <div className={styles.container}>
         <h1 className={styles.header + " text text_type_main-medium"}>Вход</h1>
-        <form onSubmit={submitHandler} className={styles.form}>
+        <form ref={formRef} onInput={inputHandler} onSubmit={submitHandler}
+              className={styles.form}>
           <Input placeholder={'E-mail'} value={value["E-mail"]} name={"E-mail"}
-                 onChange={handleChange}>
+                 onChange={handleChange} required type='email' error={error}
+                 pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$">
           </Input>
-          <Input placeholder={'Пароль'} value={value.Password} type={type} name={"Password"} onIconClick={() => {
-            setType(type === 'password' ? 'text' : 'password')
-          }}
-                 onChange={handleChange}
+          <Input placeholder={'Пароль'} required value={value.Password} type={type} name={"Password"}
+                 onIconClick={() => {
+                   setType(type === 'password' ? 'text' : 'password')
+                 }}
+                 onChange={handleChange} errorText="Неправильный пароль или e-mail" error={error}
                  icon={type === 'password' ? 'ShowIcon' : "HideIcon"}>
           </Input>
-          <Button htmlType="submit" type="primary" size="medium">
+          <Button disabled={!isFormValid} htmlType="submit" type="primary" size="medium">
             Войти
           </Button>
         </form>
